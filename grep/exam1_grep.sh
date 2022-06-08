615f8a613bc2831636b779ee00c96c5d43285883
SaborCanela <thomas.garzon@est.ikiam.edu.ec>
2022-06-08 18:37:56 -0500

    bp,cds,linear

--- grep/exam1_grep.sh
+++ grep/exam1_grep.sh
@@ -1,2 +1,6 @@
 #! bin/bash
 grep -n "complete cds" ../data/data_INFg.txt | wc -l >>cds_full.txt
+
+grep -n "linear DNA" ../data/data_INFg.txt | wc -l >>linear_dna.txt
+
+grep -w "bp"  ../data/data_INFg.txt | sort >>bp.txt
