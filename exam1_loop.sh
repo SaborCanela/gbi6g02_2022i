a770cb07f8c55c3b836a52cbd5bc3d8de03a7557
SaborCanela <thomas.garzon@est.ikiam.edu.ec>
2022-06-08 19:42:41 -0500

    loop 2

--- exam1_loop.sh
+++ exam1_loop.sh
@@ -3,9 +3,10 @@ for i in {9,19,29,39,49,59}
 do
     head -n1 exam1/multi_copia/n$i.txt | grep -o " " | wc -l
     if [$i=x]
+
         then
-        echo "Columna par"
+        echo "n$1.txt es Columna par"
         else
-        echo "columna impar"
+        echo "n$i.txt es columna impar"
         fi
 done
