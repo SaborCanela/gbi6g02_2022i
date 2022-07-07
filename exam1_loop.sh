57bda301c5f2473546f8bdfe947c271c131d8260
SaborCanela <thomas.garzon@est.ikiam.edu.ec>
2022-07-07 16:40:16 -0500

    Prueba Recuperacion

--- exam1_loop.sh
+++ exam1_loop.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-for i in {9,19,29,39,49,59}
+for f in exam1/multi_copia/*.txt #se le asigna la variable "f" a la direccion de lso diferentes archivos
 do
-    head -n1 exam1/multi_copia/n$i.txt | grep -o " " | wc -l
-    if [$i=x]
-
+        x=$( head -n1 "$f" |grep -o " " | wc -l ) #se le asigna al resultado que de una variable, el resultado es dado por el codigo, el cual cuenta las columnas de los archivos
+        if [ $(( "x" % 2 )) -eq 0 ]; #El bucle if, el cual nos dice cual es impar o par, esto por el "resto" que queda al dividirlo por 2, si queda resto es impar, si es igual a 0 es par
         then
-        echo "n$1.txt es Columna par"
+                echo "Par: $f tiene $x columnas" >> for_if.txt #Imprime los pares
         else
-        echo "n$i.txt es columna impar"
+                echo "Impar: $f tiene $x columnas"  >> for_if.txt #Imprime los impares
         fi
+
 done
